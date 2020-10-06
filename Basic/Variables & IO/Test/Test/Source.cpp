e890629c27d096eb00a4573732f3259a6efebeaa
Cristiano Caon <cristiano.caon@hotmail.com>
2020-10-05 19:56:16 -0500

    Initial commit

---
+++ Basic/Variables & IO/Test/Test/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() 
+{
+	std::cout << "Hello" << "\n";
+	std::cout << "8 * 8" << " is equal to " << 8 * 8 << "\n";
+
+	int i = 42;
+
+	std::cout << i << "\n";
+	
+	i = 300;
+	std::cout << i * 2 << "\n";
+
+	return 0;
+}
