a7fd80f1613de756c358689b8f7e055ad61bc37c
Cristiano Caon <cristiano.caon@hotmail.com>
2020-10-05 20:01:15 -0500

    Initial commit

--- Basic/Variables & IO/Test/Test/Source.cpp
+++ Basic/Variables & IO/Test/Test/Source.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+using std::cout;
+using std::cin;
 
 int main() 
 {
+	// This is a comment in C++
 	std::cout << "Hello" << "\n";
 	std::cout << "8 * 8" << " is equal to " << 8 * 8 << "\n";
 
@@ -11,6 +14,14 @@ int main()
 	
 	i = 300;
 	std::cout << i * 2 << "\n";
+
+	cout << "using std::cout\n";
+	cout << "using std::cin\n";
+
+	int number = 0;
+	cout << "Enter a number: ";
+	cin >> number;
+	cout << "The number you entered is " << number << "\n";
 
 	return 0;
 }
