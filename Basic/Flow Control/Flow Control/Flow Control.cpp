9ac48e8f9b91d767a4a654d0aa0c3a9e28bc56d5
Cristiano Caon <cristiano.caon@hotmail.com>
2020-10-06 19:19:37 -0500

    Flow Control

--- Basic/Flow Control/Flow Control/Flow Control.cpp
+++ Basic/Flow Control/Flow Control/Flow Control.cpp
@@ -9,7 +9,41 @@ int main()
 {
     // > < >= <= == !=
 
+    int firstNumber;
+    int secondNumber;
+    bool flag = true;
+    int answer;
 
+    while (flag) 
+    {
+        cout << "Enter a number: ";
+        cin >> firstNumber;
+        cout << "You entered " << firstNumber << ".\nEnter another number: ";
+        cin >> secondNumber;
+
+        if (firstNumber < secondNumber)
+        {
+            cout << "The first number, " << firstNumber << ", is less than the second number " << secondNumber << ".\n";
+        }
+        else if (firstNumber > secondNumber)
+        {
+            cout << "The first number, " << firstNumber << ", is greater than the second number " << secondNumber << ".\n";
+        }
+        else
+        {
+            cout << "The first number, " << firstNumber << ", is equal to the second number " << secondNumber << ".\n";
+        }
+
+        cout << "Do you want to compare another pair? 0 means no, 1 means years: ";
+        cin >> answer;
+        
+        if (answer == 0)
+        {
+            cout << "Exiting...\n\n";
+            flag = false;
+        }
+    }
+    
 
     return 0;
 }
